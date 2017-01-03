ffc8f7dc2575000bbe14c25d4094f9e3fd3f13ad
dscace <derek.scace@gmail.com>
2017-01-03 13:04:29 -0700

    modify routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103183917) do
+ActiveRecord::Schema.define(version: 20170103195541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170103183917) do
     t.string   "username"
     t.text     "bio"
     t.string   "location"
+    t.text     "following"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
