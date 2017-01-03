60ebfb200ba790e35dd31bd591468fa60c76fca7
dscace <derek.scace@gmail.com>
2017-01-03 11:16:01 -0700

    add values to users, add devise param sanitizers

---
+++ db/migrate/20170103181053_add_values_to_users.rb
@@ -0,0 +1,9 @@
+class AddValuesToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true
+    add_column :users, :bio, :text
+    add_column :users, :location, :string
+  end
+end
