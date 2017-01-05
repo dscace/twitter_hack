df411a39353520bbd438faf34925e9559a834355
dscace <derek.scace@gmail.com>
2017-01-05 10:55:08 -0700

    Add tag models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103195541) do
+ActiveRecord::Schema.define(version: 20170105174615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "phrase"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweet_tags", force: :cascade do |t|
+    t.integer  "tweet_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_tweet_tags_on_tag_id", using: :btree
+    t.index ["tweet_id"], name: "index_tweet_tags_on_tweet_id", using: :btree
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string   "message"
@@ -46,5 +61,7 @@ ActiveRecord::Schema.define(version: 20170103195541) do
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
+  add_foreign_key "tweet_tags", "tags"
+  add_foreign_key "tweet_tags", "tweets"
   add_foreign_key "tweets", "users"
 end
