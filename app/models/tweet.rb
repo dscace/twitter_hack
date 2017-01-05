2f56a592d04132958b06aec6d1e5c6c2cf41991b
dscace <derek.scace@gmail.com>
2017-01-05 10:59:15 -0700

    Set up many to many associations with tweet and tag

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,9 @@
 class Tweet < ApplicationRecord
   belongs_to :user
 
+  has_many :tweet_tags
+  has_many :tag, through: :tweet_tags
+
   validates :message, presence: true
   validates :message, length: {maximum: 140, 
   too_long: "A tweet is only 140 max. Everybody knows that!"}
