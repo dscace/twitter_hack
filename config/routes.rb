1df8d3f310bd2524907f49ef09b0bbf35044897d
dscace <derek.scace@gmail.com>
2017-01-20 13:44:24 -0700

    Add Feed route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get 'tag_tweets' => 'epicenter#tag_tweets'
 
+  get 'feed' => 'epicenter#feed'
+
   resources :tweets
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
